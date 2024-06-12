5b8dc1bb4683719a509ddafe25ce96f3cdde68b2
NicolasVanHoove <134655021+NicolasVanHoove@users.noreply.github.com>
2024-06-12 10:22:28 +0200

    integration-section-competence OK

--- js/switch-section-cv.js
+++ js/switch-section-cv.js
@@ -3,6 +3,8 @@ const cvNavbar = document.getElementById('section-cv');
 
 const sectionCompetence = document.getElementById('section-competence');
 
+const sectionTechno = document.getElementById('technologies');
+
 // Link selection
 const competenceLink = document.getElementById('competence-link');
 
@@ -12,5 +14,6 @@ competenceLink.onclick = displayCompetenceSection;
 // Function
 function displayCompetenceSection() {
     sectionCompetence.classList.remove('hidden');
+    sectionTechno.classList.remove('hidden');
     cvNavbar.classList.add('hidden');
 }
